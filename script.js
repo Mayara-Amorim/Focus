7f5e7ba584d6655f4de67cd2a1a1b67bb7f84b27
Mayara Amorim <mayara.moreira@fenixeditorial.com.br>
2023-10-02 16:02:40 -0300

    Commits iniciais do projeto

---
+++ script.js
@@ -0,0 +1,11 @@
+const html = document.querySelector('html')
+const focoBt = document.querySelector('.app__card-button--foco')
+const curtoBt = document.querySelector('.app__card-button--curto')
+const focoLongo = document.querySelector('.app__card-button--longo')
+
+focoBt.addEventListener('click', ()=>{
+    html.setAttribute('data-contexto', 'foco')
+})
+curtoBt.addEventListener('click', ()=>{
+    html.setAttribute('data-contexto', 'descanso-curto')
+})
